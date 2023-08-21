d407ef25da4ded3e63a183639ef2232d07856367
Majid Hasan <majidhasan@Majids-MacBook-Pro.local>
2023-08-21 11:32:14 +0600

    Testing

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,16 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
-const app: Application = express();
-const port = 3000;
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
 
-app.use(cors());
+app.use(cors())
 
 // Parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // Testing
-app.get("/", (req: Request, res: Response) => {
-  res.send("Working successfully");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Working successfully')
+})
 
-export default app;
+export default app
